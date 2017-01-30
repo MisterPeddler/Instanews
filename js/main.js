9503a10079cdbd4eee35668e7aa18e6c7dab7fd3
MisterPeddler <mr.padli@gmail.com>
2017-01-30 14:09:00 -0800

    added style to selectric to style selector

--- js/main.js
+++ js/main.js
@@ -1,56 +1,57 @@
 $(function() {
 
-//TODO fix css for drop down sliding
-//TODO sassify
-//TODO stretch goals
+    //TODO fix css for drop down sliding
+    //TODO sassify
+    //TODO stretch goals
 
-//DOM varuable declarations
-var $loader = $('.loader');
-var $errorReport = $('.error-report');
-var $dashboard = $('.dashboard');
-var $logo = $('.logo');
-var $topics = $('.topics');
-var $copyr = $('.copyr');
+    //jQuery variable declarations
+    var $loader = $('.loader');
+    var $errorReport = $('.error-report');
+    var $dashboard = $('.dashboard');
+    var $logo = $('.logo');
+    var $topics = $('.topics');
+    var $copyr = $('.copyr');
+    var $articleList = $('.article-list');
 
-var $articleList = $('.article-list');
+    //Function Declarations
+    function showArticleLayout() {
+        //reposition elements for article display
+        $dashboard.addClass('dashboard-with-articles');
+        $logo.addClass('logo-with-articles');
+        $topics.addClass('topics-with-articles');
+        $copyr.addClass('copyr-with-articles');
+    }
 
+    function showErrorMessage() {
+        $errorReport.css('display', 'inline');
+    }
 
-//Function Declarations
-function showArticleLayout(){
-  //reposition elements to article display
-  $dashboard.addClass('dashboard-with-articles');
-  $logo.addClass('logo-with-articles');
-  $topics.addClass('topics-with-articles');
-  $copyr.addClass('copyr-with-articles');
-}
+    function hideErrorMessage() {
+        $errorReport.css('display', 'none');
+    }
 
-function showErrorMessage(){
-  $errorReport.css('display','inline');
-}
+    function showLoadingWheel() {
+        $loader.css('display', 'inline');
+    }
 
-function hideErrorMessage(){
-  $errorReport.css('display','none');
-}
+    function hideLoadingWheel() {
+        $loader.css('display', 'none');
+    }
 
-function showLoadingWheel(){
-  $loader.css('display','inline');
-}
-
-function hideLoadingWheel(){
-  $loader.css('display','none');
-}
+    //initialize selectric
+    $('.drop-down-menu').selectric();
 
     $('.drop-down-menu').on('change', function() {
-      var $category = $(this).val();
-
-      showArticleLayout();
-      showLoadingWheel();
-      hideErrorMessage();
-      $articleList.empty();
-
+        var $category = $(this).val();
 
         //check to make sure we're not sending 'sections' to the NYT
         if ($category !== 'sections') {
+
+          showArticleLayout();
+          showLoadingWheel();
+          hideErrorMessage();
+          $articleList.empty();
+
             var url = 'https://api.nytimes.com/svc/topstories/v2/' + $category + '.json';
             url += '?' + $.param({
                 'api-key': '06c14a236b5b478d8ee67228dff8fc9f'
@@ -60,8 +61,8 @@ function hideLoadingWheel(){
                 url: url,
                 method: 'GET',
             }).done(function(data) {
-                var articleData = '';
 
+                var articleData = '';
 
                 var listOfArticles = data.results.filter(function(item) {
                     return item.multimedia.length > 0;
@@ -76,9 +77,10 @@ function hideLoadingWheel(){
                     articleData += '</div></li>'
                 });
 
-                hideLoadingWheel();
 
-                //load data onto the dom
+
+                //hide the loading wheel and load data onto the dom
+                hideLoadingWheel();
                 $articleList.append(articleData);
 
             }).fail(function() {
